402f66f93830a13ae38534750958e811fc6cadec
amitpal603-ops <ap036776@gmail.com>
2025-09-18 22:40:02 +0530

    about page update

--- src/components/Pages/About.jsx
+++ src/components/Pages/About.jsx
@@ -6,6 +6,7 @@ function About() {
      <div className="mt-20 bg-gradient-to-b from-black to-[#4bcdcf] h-screen">
   </div>
   <div className="h-screen bg-[#4bcdcf]">
+    Hello about
   </div>
   </div>
  
